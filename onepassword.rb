d43410a2bdd4d4f3207b007df225a71e50edcf25
Frederik Wille <frederik@wille.hamburg>
2018-07-18 19:50:44 +0200

    add basic authentication

--- onepassword.rb
+++ onepassword.rb
@@ -2,11 +2,18 @@
 # frozen_string_literal: true
 
 require "json"
+require "byebug"
 require "clipboard"
+require "active_support/all"
 
 ACTIONS = ["Copy", "Open"].freeze
 
+OP_ADDRESS = ENV["OP_ADDRESS"]
+OP_EMAIL = ENV["OP_EMAIL"]
+OP_SECRET_KEY = ENV["OP_SECRET_KEY"]
+
 def main
+  check_login_and_authenticate
   short_items = get_short_items
   short_item = choose_item(short_items)
   chosen_item = get_full_item(short_item)
@@ -59,6 +66,31 @@ def get_short_items
     mapped[title] = item
     mapped
   end
+end
+
+def check_login_and_authenticate
+  _, error, _ = Open3.capture3("op get item a")
+  if error =~ /You are not currently signed in./
+    authenticate_1p!
+  end
+end
+
+def authenticate_1p!
+  password = dmenu_password
+  if password.present?
+    output = `op signin #{OP_ADDRESS} #{OP_EMAIL} #{OP_SECRET_KEY} #{password}`
+    session_key = output.match(/\"(.*)\"/)&.captures&.first
+    if session_key.blank?
+      authenticate_1p!
+    end
+    ENV["OP_SESSION_my"] = session_key
+  else
+    exit
+  end
+end
+
+def dmenu_password
+  `dmenu -p \"1Password Master Password\" -fn 'Consolas:size=10' -nf '#204a87' -nb '#204a87' -sf black -sb '#babdb6' <&-`
 end
 
 main
